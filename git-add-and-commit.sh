f5037f442c3ae4c5f951f5cd06175a0287558bae
George <georgedot@gmail.com>
2013-10-05 19:20:16 +0400

    Fixed commit script

--- git-add-and-commit.sh
+++ git-add-and-commit.sh
@@ -67,9 +67,9 @@ if promptynY "Ready to begin commiting new version [Y|n]"; then
         echo
         git commit -a
 
-        if promptynN "$ git push --all --tags [y|N]: "; then
+        if promptynN "$ git push --all [y|N]: "; then
             echo
-            git push --all --tags
+            git push --all
         fi
 
     fi
