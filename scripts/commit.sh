51ca464c5f80bbe30de7e732bc6cfdfe351c2a14
root <root@hertz-lx.(none)>
2013-10-05 16:48:31 +0400

    asd

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-git status
+STATUS_OUTPUT="$(git status -s)"
+STATUS_OUTPUT_LENGTH=expr length "$STATUS_OUTPUT"
+
+if ["$STATUS_OUTPUT_LENGTH"=="0"]; then
+    echo "OK\n"
+else
+    echo "LENGTH > 0"
+fi
