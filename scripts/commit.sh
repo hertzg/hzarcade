78cd077df6e3a875dae0129391d99db13a432324
root <root@hertz-lx.(none)>
2013-10-05 16:57:03 +0400

    asd

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-function commit {
-    echo "Commiting"
-}
-
 STATUS_OUTPUT="$(git status -s)"
 STATUS_OUTPUT_LENGTH=`expr length "$STATUS_OUTPUT"`
 
+function generateVersionFile {
+    echo `./genVersion.php` > ../VERSION
+    echo "DONE!"
+}
+
+
 if [ $STATUS_OUTPUT_LENGTH -eq 0 ]; then
-    commit
+    generateVersionFile
 else
     echo "There are some uncommited changes"
 fi
